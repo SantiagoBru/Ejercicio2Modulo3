f92c3f800e86b50b55ee4fa8d00aa57efb72c3da
SantiagoBru <106764977+SantiagoBru@users.noreply.github.com>
2024-08-13 09:32:27 -0300

    Add files via upload

---
+++ Ejercicio2Modulo3/EjercicioModulo3Clase2/Controllers/TaskController.cs
@@ -0,0 +1,75 @@
+﻿using EjercicioModulo3Clase2.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+using EjercicioModulo3Clase2.Services.Interfaces;
+
+namespace EjercicioModulo3Clase2.Controllers
+{
+    [Route("api/task")]
+    [ApiController]
+    public class TaskController : ControllerBase
+    {
+        #region Pasos previos
+        /*
+         * 1 - Instalar EF Core y EF Core SQL Server
+         * 2 - Crear contexto de base de datos y los modelos. Se puede usar Ingenieria Inversa de EF Core Power Tools
+         * 3 - Configurar la inyección de dependencia del contexto tanto en Program.cs como en el controlador. No olvidar el string de conexión.
+         * 4 - Las rutas de los endpoints queda a criterio de cada uno.
+         * 5 - En este controlador, realizar los siguientes ejercicios:
+         */
+        #endregion
+        private ITasksService _clienteService;
+
+        public TaskController(ITasksService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        #region Ejercicio 1
+        // Crear un endpoint para obtener un listado de todas las tareas usando HTTP GET
+        [HttpGet]
+
+        public ActionResult getTask()
+        {
+            return Ok(_clienteService.getTasks());
+        }
+        #endregion
+
+        #region Ejercicio 2
+        // Crear un endpoint para obtener una tarea por ID usando HTTP GET
+        [HttpGet("/{id}")]
+        public ActionResult getTaskById([FromRoute] int id)
+        {
+            return Ok(_clienteService.getTaskById(id));
+        }
+        #endregion
+
+        #region Ejercicio 3
+        // Crear un endpoint para crear una nueva tarea usando HTTP POST
+        [HttpPost]
+        public ActionResult saveTask([FromBody] Tasks task)
+        {
+            _clienteService.saveTask(task);
+            return Ok();
+        }
+        #endregion
+
+        #region Ejercicio 4
+        // Crear un endpoint para marcar una tarea como completada usando HTTP PUT
+        [HttpPut("/completar/{id}")]
+        public ActionResult putCompleteTask([FromRoute] int id)
+        { 
+            return Ok(_clienteService.putCompleteTask(id));                                                         
+        }
+        #endregion
+
+        #region Ejercicio 5
+        // Crear un endpoint para dar de baja una tarea usando HTTP PUT (baja lógica)
+        [HttpPut ( "/baja/{id}" )]
+        public ActionResult putDarBaja([FromRoute] int id)
+
+        {
+            return Ok(_clienteService.putDarBaja(id));
+        }       
+    }
+    #endregion
+}
---
+++ Ejercicio2Modulo3/EjercicioModulo3Clase2/Program.cs
@@ -0,0 +1,48 @@
+using EjercicioModulo3Clase2.Repository;
+using EjercicioModulo3Clase2.Services;
+using EjercicioModulo3Clase2.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EjercicioModulo3Clase2
+{
+    public class Program
+    {
+        public static void Main( string[] args )
+        {
+            var builder = WebApplication.CreateBuilder( args );
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            String connection =  builder.Configuration.GetConnectionString("DefaulConnection");
+            builder.Services.AddDbContext<ToDoListDBContext>(opt =>
+            {
+                opt.UseSqlServer(connection);
+            });
+
+            builder.Services.AddScoped<ITasksService, TasksService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
